836cd5be18228696ea365f6b78ab27fa1e8bdd45
chenhaidong <chenhaidong1@xiaomi.com>
2019-03-07 13:53:03 +0800

    solve one question

--- src/leetcode/No003.java
+++ src/leetcode/No003.java
@@ -32,9 +32,10 @@ public class No003 {
   public static void main(String[] args) {
 //    System.out.println(lengthOfLongestSubstring("pwwkew"));
 //    System.out.println(lengthOfLongestSubstring2("pwwkew"));
-    System.out.println(lengthOfLongestSubstring3("tmmzuxt"));
-    System.out.println(lengthOfLongestSubstring3("wpw"));
-    System.out.println(lengthOfLongestSubstring3(" "));
+//    System.out.println(lengthOfLongestSubstring3("tmmzuxt"));
+//    System.out.println(lengthOfLongestSubstring3("wpw"));
+//    System.out.println(lengthOfLongestSubstring3(" "));
+    System.out.println(lengthOfLongestSubstringTwoDistinct("pwwwke"));
   }
 
   private static int lengthOfLongestSubstring(String s) {
@@ -93,5 +94,22 @@ public class No003 {
     }
 
     return result;
+  }
+
+  private static int lengthOfLongestSubstringTwoDistinct(String s) {
+    int[] count = new int[256];
+    int i = 0, numDistinct = 0, maxLen = 0;
+    for (int j = 0; j < s.length(); j++) {
+      if (count[s.charAt(j)] == 0) numDistinct++;
+      count[s.charAt(j)]++;
+      while (numDistinct > 2) {
+        count[s.charAt(i)]--;
+        if (count[s.charAt(i)] == 0) numDistinct--;
+        i++;
+      }
+      maxLen = Math.max(j - i + 1, maxLen);
+      System.out.println(j + " >>> " + s.substring(i, j + 1) + ", len: " + maxLen);
+    }
+    return maxLen;
   }
 }
