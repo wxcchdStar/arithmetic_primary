2f130c88644c1c0bc95410ebc58b87089ebec42f
chenhaidong <chenhaidong1@xiaomi.com>
2019-03-17 11:44:37 +0800

    solve one question

---
+++ src/leetcode/No198.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 打家劫舍
+ */
+public class No198 {
+
+  public static void main(String[] args) {
+    System.out.println(rob(new int[]{2, 1, 1, 2}));
+  }
+
+  public static int rob(int[] nums) {
+    if (nums.length == 0) return 0;
+    if (nums.length == 1) return nums[0];
+
+    int[] dp = new int[nums.length];
+    for (int i = 0; i < nums.length; i++) {
+      if (i == 0 || i == 1) {
+        dp[i] = nums[i];
+      } else if (i == 2) {
+        dp[i] = nums[i] + dp[i - 2];
+      } else {
+        dp[i] = nums[i] + Math.max(dp[i - 2], dp[i - 3]);
+      }
+    }
+    System.out.println(Arrays.toString(dp));
+    return Math.max(dp[nums.length - 1], dp[nums.length - 2]);
+  }
+}
