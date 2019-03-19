e7baa10d2ae86212710f1ab130580fdf73cb16d3
chenhaidong <chenhaidong1@xiaomi.com>
2019-03-19 10:08:35 +0800

    solve one question

--- src/leetcode/No055.java
+++ src/leetcode/No055.java
@@ -23,19 +23,50 @@ package leetcode;
 public class No055 {
 
   public static void main(String[] args) {
-    System.out.println(canJump(new int[]{2, 3, 1, 1, 4}));
-    System.out.println(canJump(new int[]{3, 2, 1, 0, 4}));
+    System.out.println(canJump2(new int[]{2, 3, 1, 1, 4}));
+    System.out.println(canJump2(new int[]{3, 2, 1, 0, 4}));
+    System.out.println(canJump2(new int[]{2, 0}));
+  }
+
+  private static boolean canJump2(int[] nums) {
+    int result = nums.length - 1;
+    for (int i = nums.length - 1; i >= 0; i--) {
+      if (i + nums[i] >= result) {
+        result = i;
+      }
+    }
+    return result == 0;
   }
 
   private static boolean canJump(int[] nums) {
+    result = new int[nums.length];
+    result[nums.length - 1] = 1;
     return test(nums, 0);
   }
 
+  static int[] result;
+
   private static boolean test(int[] nums, int start) {
+    if (result[start] != 0) {
+      return result[start] == 1;
+    }
+    for (int i = 1; start + i < nums.length && i <= nums[start]; i++) {
+      if (test(nums, start + i)) {
+        result[i] = 1;
+        return true;
+      }
+    }
+    result[start] = 2;
+    return false;
+  }
+
+  private static boolean test2(int[] nums, int k) {
+    if (k == 0) return nums.length <= 1 || nums[0] > 0;
+
     boolean result = false;
-    if (nums[start] > 0) {
-      for (int i = 1; start < nums.length - 1 && i <= nums[start]; i++) {
-        result |= test(nums, start + i);
+    for (int i = 1; i <= k; i++) {
+      if (nums[k - i] >= i) {
+        result |= test2(nums, k - i);
       }
     }
     return result;
