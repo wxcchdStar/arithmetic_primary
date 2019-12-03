cefceb21acadb55cbf8501c5a60fa768a1249491
chenhaidong <chenhaidong@xiaomi.com>
2019-12-03 16:02:42 +0800

    fix a bug

---
+++ src/leetcode/No048.java
@@ -0,0 +1,108 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 48. 旋转图像
+ * <p>
+ * 给定一个 n × n 的二维矩阵表示一个图像。
+ * <p>
+ * 将图像顺时针旋转 90 度。
+ * <p>
+ * 说明：
+ * <p>
+ * 你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 给定 matrix =
+ * [
+ * [1,2,3],
+ * [4,5,6],
+ * [7,8,9]
+ * ],
+ * <p>
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ * [7,4,1],
+ * [8,5,2],
+ * [9,6,3]
+ * ]
+ * 示例 2:
+ * <p>
+ * 给定 matrix =
+ * [
+ * [ 5, 1, 9,11],
+ * [ 2, 4, 8,10],
+ * [13, 3, 6, 7],
+ * [15,14,12,16]
+ * ],
+ * <p>
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ * [15,13, 2, 5],
+ * [14, 3, 4, 1],
+ * [12, 6, 8, 9],
+ * [16, 7,10,11]
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/rotate-image
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class No048 {
+
+  public static void main(String[] args) {
+    int[][] matrix = new int[][]{
+        {5, 1, 9, 11},
+        {2, 4, 8, 10},
+        {13, 3, 6, 7},
+        {15, 14, 12, 16}
+    };
+    rotate2(matrix);
+    print(matrix);
+  }
+
+  private static void rotate(int[][] matrix) {
+    int n = matrix.length;
+
+    // transpose matrix
+    for (int i = 0; i < n; i++) {
+      for (int j = i; j < n; j++) {
+        int tmp = matrix[j][i];
+        matrix[j][i] = matrix[i][j];
+        matrix[i][j] = tmp;
+      }
+    }
+    print(matrix);
+    // reverse each row
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n / 2; j++) {
+        int tmp = matrix[i][j];
+        matrix[i][j] = matrix[i][n - j - 1];
+        matrix[i][n - j - 1] = tmp;
+      }
+    }
+    print(matrix);
+  }
+
+  public static void rotate2(int[][] matrix) {
+    int n = matrix.length;
+    for (int i = 0; i < (n + 1) / 2; i++) {
+      for (int j = 0; j < n / 2; j++) {
+        int temp = matrix[n - 1 - j][i];
+        matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+        matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+        matrix[j][n - 1 - i] = matrix[i][j];
+        matrix[i][j] = temp;
+      }
+    }
+  }
+
+  private static void print(int[][] matrix) {
+    for (int i = 0; i < matrix[0].length; i++) {
+      System.out.println(Arrays.toString(matrix[i]));
+    }
+    System.out.println("");
+  }
+}
