27e0395f37b12d57b63d9d2243cda2d8c5fa8fdc
chenhaidong <chenhaidong@xiaomi.com>
2019-12-09 17:50:17 +0800

    solve two question

---
+++ src/leetcode/No035.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+/**
+ * 35. 搜索插入位置
+ */
+public class No035 {
+
+  public static void main(String[] args) {
+    System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 0));
+  }
+
+  public static int searchInsert(int[] nums, int target) {
+    int start = 0;
+    int end = nums.length - 1;
+    while (start < end) {
+      int index = (end + start) / 2;
+      if (nums[index] == target) {
+        return index;
+      } else if (nums[index] > target) {
+        end = index - 1;
+      } else {
+        start = index + 1;
+      }
+    }
+    return nums[start] < target ? start + 1 : start;
+  }
+}
