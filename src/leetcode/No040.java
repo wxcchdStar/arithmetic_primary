34a986af41773a834753848827abdf39e10db892
chenhaidong1 <chenhaidong1@xiaomi.com>
2020-03-06 17:08:47 +0800

    solve one problem

--- src/leetcode/No040.java
+++ src/leetcode/No040.java
@@ -39,7 +39,7 @@ import java.util.List;
  * 链接：https://leetcode-cn.com/problems/combination-sum-ii
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class No40 {
+public class No040 {
 
   public static void main(String[] args) {
     List<List<Integer>> result = combinationSum2(new int[]{10, 1, 2, 7, 6, 1, 5}, 8);
